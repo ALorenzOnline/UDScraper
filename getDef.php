957ab987bdd318ef0e1f6f013600ce7847aaa89b
Orebix <aal33@njit.edu>
2016-01-03 00:54:28 -0500

    added a definition scraper for the given term

---
+++ getDef.php
@@ -0,0 +1,33 @@
+<?php
+try{
+	
+	
+	function getDef($term)
+	{
+        	include 'storeTerms.php';
+		$url="http://www.urbandictionary.com/define.php?term=$term";
+		$output=file_get_contents($url);
+		$file="tempDef.txt";
+		file_put_contents($file,$output,FILE_APPEND);
+
+		$contents= file_get_contents($file);
+
+		//Gets and entire string  by searching for 
+		//the quote below and sorting them into an array
+		$pattern=preg_quote('/<div class="meaning">(.*?)<\/div>/s',"/");
+		$pattern = "/^.*$pattern.*\$/m";
+		preg_match("/<div class='meaning'.*?>([^`]*?)<\/div>/",$contents,$matches);
+		var_dump($matches);
+		
+		//foreach($matches as $def)
+		//{
+			//echo $def;		
+		//}
+	}
+	getDef("daddy+talk");
+	
+}catch(ErrorException $e){
+	echo $e->getMessage();
+}
+?>
+?>
